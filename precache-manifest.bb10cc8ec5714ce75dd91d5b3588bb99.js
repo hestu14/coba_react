af9acbe9cf48929964f64296e185eed08f22ec0a
hestu14 <hestu.14@gmail.com>
2020-05-27 21:32:17 +0700

    Updates

---
+++ precache-manifest.bb10cc8ec5714ce75dd91d5b3588bb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ece5b240634bcd77fb0fbe71d01fcf9",
+    "url": "/coba_react/index.html"
+  },
+  {
+    "revision": "a278c736cbf75c33e5a0",
+    "url": "/coba_react/static/css/main.54e1ebc4.chunk.css"
+  },
+  {
+    "revision": "3f35e02b2a842b72bd32",
+    "url": "/coba_react/static/js/2.3f3d6274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coba_react/static/js/2.3f3d6274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a278c736cbf75c33e5a0",
+    "url": "/coba_react/static/js/main.57fa738f.chunk.js"
+  },
+  {
+    "revision": "41f0150a251c3d39b0a3",
+    "url": "/coba_react/static/js/runtime-main.6a09616c.js"
+  }
+]);
